e2690c9db01f707196e7603cb5fa6c2f2535148c
Wasyl Mowczan <mowczanwasyl@gmail.com>
2021-04-26 16:13:41 +0200

    changes in Setting Driver

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -8,15 +8,30 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeEach
-    public void testSetUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void testSetUp() throws MalformedURLException {
+        final String USERNAME = "YOUR_USERNAME";
+        final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+        String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("version", "latest");
+
+        driver = new RemoteWebDriver(new URL(URL), caps);
+
+        //WebDriverManager.chromedriver().setup();
+        //driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
         driver.manage().window().setSize(new Dimension(1290, 730));
