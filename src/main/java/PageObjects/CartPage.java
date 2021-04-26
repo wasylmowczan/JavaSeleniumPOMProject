ff9c254f951b19a74aade869b14daabf2938dc4f
Wasyl Mowczan <mowczanwasyl@gmail.com>
2021-04-26 04:41:11 +0200

    Add common page: HeaderPage

--- src/main/java/PageObjects/CartPage.java
+++ src/main/java/PageObjects/CartPage.java
@@ -5,23 +5,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CartPage extends BasePage {
-    private final By shopTableLocator = By.cssSelector(".shop_table");
-    private final By productQuantityFieldLocator = By.cssSelector("div.quantity>input");
-    private final String removeProductButtonCssSelector = "a[data-product_id='<product_id>']";
+public class CartPage extends BasePage{
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
-    public int getProductsAmount(String productId) {
-        WebDriverWait wait = new WebDriverWait(driver, 7);
-        wait.until(ExpectedConditions.presenceOfElementLocated(shopTableLocator));
-        By removeProductLocator = By.cssSelector(removeProductButtonCssSelector.replace("<product_id>", productId));
-        return driver.findElements(removeProductLocator).size();
-    }
+    private By shopTableLocator = By.cssSelector(".shop_table");
+    private By productQuantityFieldLocator = By.cssSelector("div.quantity>input");
+    private String removeProductButtonCssSelector = "a[data-product_id='<product_id>']";
+    private By cartItemLocator = By.cssSelector(".cart_item");
 
     public int getProductQuantity() {
+        waitForShopTable();
         String quantityString = driver.findElement(productQuantityFieldLocator).getAttribute("value");
         return Integer.parseInt(quantityString);
     }
+
+    public boolean isProductInCart(String productId) {
+        waitForShopTable();
+        By removeProductLocator = By.cssSelector(removeProductButtonCssSelector.replace("<product_id>", productId));
+        int productRecords = driver.findElements(removeProductLocator).size();
+        boolean presenceOfProduct = false;
+        if (productRecords==1){
+            presenceOfProduct = true;
+        } else if (productRecords>1){
+            throw new IllegalArgumentException("There is more than one record for the product in cart.");
+        }
+        return presenceOfProduct;
+    }
+
+    public int getNumberOfProducts() {
+        waitForShopTable();
+        return driver.findElements(cartItemLocator).size();
+    }
+
+    private void waitForShopTable(){
+        WebDriverWait wait = new WebDriverWait(driver, 7);
+        wait.until(ExpectedConditions.presenceOfElementLocated(shopTableLocator));
+    }
+
 }
--- src/test/java/Tests/CartTest.java
+++ src/test/java/Tests/CartTest.java
@@ -1,43 +1,64 @@
-package Tests;
+package TestyPOM;
 
 import PageObjects.CategoryPage;
 import PageObjects.ProductPage;
+import Tests.BaseTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CartTest extends BaseTest {
+
     String productId = "386";
     String productUrl = "https://fakestore.testelka.pl/product/egipt-el-gouna/";
     String categoryURL = "https://fakestore.testelka.pl/product-category/windsurfing/";
+    String[] productPages = {"/egipt-el-gouna/","/wspinaczka-via-ferraty/","/wspinaczka-island-peak/",
+            "/fuerteventura-sotavento/", "/grecja-limnos/", "/windsurfing-w-karpathos/",
+            "/wyspy-zielonego-przyladka-sal/", "/wakacje-z-yoga-w-kraju-kwitnacej-wisni/",
+            "/wczasy-relaksacyjne-z-yoga-w-toskanii/", "/yoga-i-pilates-w-hiszpanii/"};
 
     @Test
-    public void addToCartFromProductPageTest() {
+    public void addToCartFromProductPageTest(){
         ProductPage productPage = new ProductPage(driver).goTo(productUrl);
         productPage.closeDemoNotice();
-        int productAmount = productPage.addToCart().viewCart().getProductsAmount(productId);
-        assertTrue(productAmount == 1,
-                "Remove button was not found for a product with id=386 (Egipt - El Gouna). " +
+        boolean isProductInCart = productPage.addToCart().viewCart().isProductInCart(productId);
+
+        assertTrue(isProductInCart,
+                "Remove button was not found for a product with id=" + productId + ". " +
                         "Was the product added to cart?");
     }
 
     @Test
-    public void addToCartFromCategoryPageTest() {
+    public void addToCartFromCategoryPageTest(){
         CategoryPage categoryPage = new CategoryPage(driver).goTo(categoryURL);
         categoryPage.closeDemoNotice();
-        int productAmount = categoryPage.addToCart(productId).viewCart().getProductsAmount(productId);
-        assertTrue(productAmount == 1,
-                "Remove button was not found for a product with id=386 (Egipt - El Gouna). " +
+        boolean isProductInCart = categoryPage.addToCart(productId).viewCart().isProductInCart(productId);
+
+        assertTrue(isProductInCart,
+                "Remove button was not found for a product with  id=" + productId + ". " +
                         "Was the product added to cart?");
     }
 
     @Test
-    public void addOneProductTenTimesTest() {
+    public void addOneProductTenTimesTest(){
         ProductPage productPage = new ProductPage(driver).goTo(productUrl);
         productPage.closeDemoNotice();
         int productQuantity = productPage.addToCart(10).viewCart().getProductQuantity();
+
         assertEquals(10, productQuantity,
                 "Quantity of the product is not what expected. Expected: 10, but was " + productQuantity);
+    }
+
+    @Test
+    public void addTenProductsToCartTest(){
+        ProductPage productPage = new ProductPage(driver);
+        for (String product: productPages) {
+            productPage.goTo("https://fakestore.testelka.pl/product" + product).addToCart();
+        }
+        int numberOfItems = productPage.header.viewCart().getNumberOfProducts();
+
+        assertEquals(10, numberOfItems,
+                "Number of items in the cart is not correct. Expected: 10, but was: " + numberOfItems);
     }
 }
